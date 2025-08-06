af26dad28cac340b4ac1672f05ad9798151a9872
Samarth <98687409+SamarthM0419@users.noreply.github.com>
2025-08-06 19:46:33 +0530

    PostOrderTraversal

---
+++ PostOrderTraversal.js
@@ -0,0 +1,15 @@
+// Post Order Traversal = left - right - root
+
+var postorderTraversal = function(root) {
+    let ans = [];
+
+    function traversal(curr){
+        if(!curr) return;
+        traversal(curr.left);
+        traversal(curr.right);
+
+        ans.push(curr.val);
+    }
+    traversal(root);
+    return ans;
+};

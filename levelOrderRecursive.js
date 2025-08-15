61a959a78a4d64aaf8fa05b7e34cf2612c0c1875
Samarth <98687409+SamarthM0419@users.noreply.github.com>
2025-08-15 10:02:57 +0530

    Create levelOrderRecursive.js

---
+++ levelOrderRecursive.js
@@ -0,0 +1,14 @@
+
+
+var levelOrder = function(root) {
+    if(!root) return [];
+    let ans = [];
+    let traversal = (curr, level) => {
+        if(!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+    traversal(root, 0);
+    return ans;
+};
